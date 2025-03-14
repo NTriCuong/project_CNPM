358aa4ed6228eefa6fe8c459b40743630470c192
Nguyen Tri Cuong <nguyentricuong1504@gmail.com>
2025-03-14 23:17:13 +0700

    đã sữa giao diện sẳng sàng buid xong chức năgn tuần này

--- src/page/flightLogin/FlightBookingLogin.js
+++ src/page/flightLogin/FlightBookingLogin.js
@@ -91,16 +91,18 @@ const FlightBookingLogin = ({className, onLogin, create}) => {
       <div className="footer-info">
         <div className="flight-options">
             
-              <div style={{color:'#EAF0F0', display:'inline-block',
-                marginLeft:'0px' ,width:'220px', position:'relative',textAlign:'end'}}>
-              <img src={calendar} alt='calendar' style={{width:'20px', position:'absolute',left:'40px',top:'2px'}}/>
+              <div style={{color:'#EAF0F0', display:'inline-block',fontSize:'12px',
+                marginLeft:'0px' ,width:'170px', position:'relative',textAlign:'end'}}>
+              <img src={calendar} alt='calendar' style={{width:'20px', position:'absolute',left:'18px',top:'-1px'}}/>
                 Xem/Hủy Chuyến Bay</div>
+
             <hr style={{margin:'10px', width:'2px', height:'30px', border:'none',
             background: 'linear-gradient(to bottom, transparent, #EAF0F0, transparent)'}}/>
-            <div style={{color:'#EAF0F0',position:'relative',width:'220px',marginLeft:'22px'}}>
-            <img src={history} alt='history' style={{width:'20px', position:'absolute',left:'-22px',top:'2px'}}/>
+            <div style={{color:'#EAF0F0',position:'relative',width:'170px',marginLeft:'22px',fontSize:'12px'}}>
+            <img src={history} alt='history' style={{width:'20px', position:'absolute',left:'-23px',top:'-1px'}}/>
               Kiểm Tra Lịch sử Chuyến Bay</div>
         </div>
+        
             <hr style={{margin:'10px', height:'2px', border:'none',
             background: 'linear-gradient(to right, transparent, #EAF0F0, transparent)'}}/>
         <p className="company-description" style={{color:'#EAF0F0'}}>
