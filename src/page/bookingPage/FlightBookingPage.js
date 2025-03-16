73a4160dac039b4818a71d4e8f38948b8821e7d9
minhnhan1011 <lieuminhnhan1011@gmail.com>
2025-03-16 23:11:34 +0700

    them filter

--- src/page/bookingPage/FlightBookingPage.js
+++ src/page/bookingPage/FlightBookingPage.js
@@ -1,4 +1,13 @@
-function FlightBookingPage(){
-    return <div> FlightBookingPage</div>
+import Menu from "../../component/menu/Menu";
+import FlightSearchBox from "../../component/search/FlightSearchBox";
+import FlightFilters from "./component/FlightFilters";
+import './FlightBookingPage.css';
+
+
+function FlightBookingPage() {
+    return <div> 
+        <Menu className='booking-header'/>
+        <FlightSearchBox/>
+    </div>
 }
 export default FlightBookingPage;
