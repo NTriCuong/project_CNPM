b9b458f1b3eb62338f8c03611beb668171e3c354
minhnhan1011 <lieuminhnhan1011@gmail.com>
2025-04-05 19:49:50 +0700

    Booking page 80%

--- src/page/bookingPage/component/FlightList.js
+++ src/page/bookingPage/component/FlightList.js
@@ -5,15 +5,15 @@ import FlightDetails from "./list/FlightDetails"
 
 const go = [
   {
-    diemden : "Hồ Chí Minh",
+    diemden: "Hồ Chí Minh",
     code1: "SGN",
-    diemdi : "Hà Nội",
+    diemdi: "Hà Nội",
     code2: "HAN"
   },
   {
-    diemden : "Hồ Chí Minh",
+    diemden: "Hồ Chí Minh",
     code1: "SGN",
-    diemdi : "Hà Nội",
+    diemdi: "Hà Nội",
     code2: "HAN"
   }
 ]
@@ -23,31 +23,30 @@ function Flightlist() {
       {go.map(item => {
         return (
           <div className="flight-list-container">
-        {/* Outbound Flights Section */}
-        <div className="flight-section">
-          <div className="route-header">
-            <div className="route-info">
-              <div className="route-location">
-                <h2>{item.diemden}</h2>
-                <div className="airport-code">{item.code1}</div>
-              </div>
-              <div className="route-arrow">⟶</div>
-              <div className="route-location">
-                <h2>{item.diemdi}</h2>
-                <div className="airport-code">{item.code2}</div>
+            {/* Outbound Flights Section */}
+            <div className="flight-section">
+              <div className="route-header">
+                <div className="route-info">
+                  <div className="route-location">
+                    <h2>{item.diemden}</h2>
+                    <div className="airport-code">{item.code1}</div>
+                  </div>
+                  <div className="route-arrow">⟶</div>
+                  <div className="route-location">
+                    <h2>{item.diemdi}</h2>
+                    <div className="airport-code">{item.code2}</div>
+                  </div>
+                </div>
+                <button className="collapse-button">
+                  <span className="arrow-up">∧</span>
+                </button>
               </div>
             </div>
-            <button className="collapse-button">
-              <span className="arrow-up">∧</span>
-            </button>
+            <DateSelector />
+            <FlightDetails />
           </div>
-        </div>
-        <DateSelector/>
-        <FlightDetails/>
-      </div>
         )
       })}
-      
     </div>
   );
 }
