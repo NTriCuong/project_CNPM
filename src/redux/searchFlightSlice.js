c0b2c2092a91fcb076a321580149fd969e85ce73
Minh Nhan <lieuminhnhan1011@gmail.com>
2025-04-25 01:46:29 +0700

    Goi API BookingPage

---
+++ src/redux/searchFlightSlice.js
@@ -0,0 +1,18 @@
+// redux/flightSearchSlice.js
+import { createSlice } from "@reduxjs/toolkit";
+
+const flightSearchSlice = createSlice({
+  name: "flightSearch",
+  initialState: {
+    data: [
+    ],
+  },
+  reducers: {
+    setFlightData: (state, action) => {
+      state.data = action.payload;
+    },
+  },
+});
+
+export const { setFlightData } = flightSearchSlice.actions;
+export default flightSearchSlice.reducer;
