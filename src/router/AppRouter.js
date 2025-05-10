7e86eb41e023a0a9865bdcd6ba379bf31679a301
Nguyen Tri Cuong <nguyentricuong1504@gmail.com>
2025-05-11 01:16:34 +0700

    xây dựng trang admin

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from '../page/homePage/HomePage';
 import FlightBookingPage from'../page/bookingPage/FlightBookingPage';
 import FinalQuestion from "../page/finalQuestion/FinalQuestion";
+import AdminPage from "../page/adminPage/AdminPage";
+import Form from "../page/adminPage/component/Form";
 function AppRouter() {
   return (
     <Router>
@@ -9,6 +11,9 @@ function AppRouter() {
         <Route path="/" element={<HomePage/>} />
         <Route path="/booking" element={<FlightBookingPage />} />
         <Route path="/final-question" element={<FinalQuestion/>} />
+        <Route path="/admin" element={<AdminPage/>} />
+        <Route path="/admin/form" element={<Form/>} />
+        <Route path="*" element={<div>404 Not Found</div>} />
       </Routes>
     </Router>
   );
