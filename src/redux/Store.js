c8b40d004c59ae31568f934d4831439678c2bacb
Nguyen Tri Cuong <nguyentricuong1504@gmail.com>
2025-05-13 18:53:21 +0700

    sữa lỗi local

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -4,8 +4,8 @@ import  stOtp  from './stOtpSlice';
 import  statusClient  from './statusClient';
 import searchDataClice from './searchDataClice';
 import flightSearchSlice from './searchFlightSlice';
-import { setDataClientDisplay } from './dataClientDisplay';
 import dataDisplay from './dataDisplay';
+import { dataClientDisplay } from './dataClientDisplay';
 
 const store = configureStore({
   reducer: {
@@ -17,7 +17,7 @@ const store = configureStore({
     statusClient:statusClient,
     searchDataClice:searchDataClice,
     searchFlight: flightSearchSlice,
-    setDataClientDisplay:setDataClientDisplay,
+    dataClientDisplay:dataClientDisplay,
     dataDisplay: dataDisplay 
   }
 });
@@ -27,6 +27,6 @@ export const selecStatusOtp = (state)=>state.statusOtp.flag
 export const selecStatusClient = (state)=>state.statusClient?.status || ''
 export const selectSearchData = (state)=>state.searchDataClice.data;
 export const selectSearchFlight = (state)=>state.searchFlight.data || [];
-export const selectDataClientDisplay = (state) => state.setDataClientDisplay;
+export const selectDataClientDisplay = (state) => state.dataClientDisplay;
 export const selectDataDisplay = (state) => state.dataDisplay.data;
 export default store;
